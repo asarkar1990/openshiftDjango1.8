0211dc557b37d01ee4d09787da1bfc7bd7fe33bf
arindam <asarkar1990@gmail.com>
2015-06-30 17:36:49 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='onlinejobcart',
+    version='1.0',
+    description='django on OpenShift',
+    author='Arindam Sarkar',
+    author_email='asarkar1990@gmail.com',
+    url='http://onlinejobcart.com',
+    install_requires=['Django==1.8',],
+)
